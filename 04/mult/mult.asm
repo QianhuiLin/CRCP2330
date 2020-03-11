ff27354d09b8f1a5657c8ea125020c9d12dd08b5
QianhuiLin <32785919+QianhuiLin@users.noreply.github.com>
2020-03-11 03:08:59 -0500

    Fix formatting and add some comments.

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -11,39 +11,51 @@
 
 // for(int i - 1; i <= R0; i++){
 //    R2 = R1 + R2}
-@i 
-//memory lovation
-M=0   
-// let i = 0
+       @i 
+       //memory lovation
+       M=0   
+       // let i = 0
 
-@result 
-// refers to another location
-M=0
+       @result 
+       // refers to another location
+       M=0
 
 //LOOP
+
 (LOOP)
         @i
         D=M 
+		// D = i;
         @R1
         D=D-M 
+		//D = i - R1;
         @END
         D;JEQ 
+		// if(i -100)> 0 goto  END;
 
 
         @result
         D=M 
+		// D = i
         @R0
         D=D+M
         @result
         M=D 
+		// result = result + b;
+
         @i
         M=M+1 
+		// i = i+ 1;
         @LOOP
         0;JMP    
+		// goto LOOP
+
 (END)
         @result
         D=M
         @R2
         M=D 
+		//R2 = result;
+
         @END
         0;JMP 
