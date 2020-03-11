65714e59d3ef9c06239342e6eaa83d5e48b3d07d
QianhuiLin <32785919+QianhuiLin@users.noreply.github.com>
2020-03-11 02:57:43 -0500

    Upload mult.asm
    Tested and stores the result in R2 as R0 and R1's multiplication.

---
+++ mult.asm
@@ -0,0 +1,49 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Put your code here.
+
+
+// for(int i - 1; i <= R0; i++){
+//    R2 = R1 + R2}
+@i 
+//memory lovation
+M=0   
+// let i = 0
+
+@result 
+// refers to another location
+M=0
+
+//LOOP
+(LOOP)
+        @i
+        D=M 
+        @R1
+        D=D-M 
+        @END
+        D;JEQ 
+
+
+        @result
+        D=M 
+        @R0
+        D=D+M
+        @result
+        M=D 
+        @i
+        M=M+1 
+        @LOOP
+        0;JMP    
+(END)
+        @result
+        D=M
+        @R2
+        M=D 
+        @END
+        0;JMP 
